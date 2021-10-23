d8ea5014911a008f73862efb0d920f5d0aa15414
E <saiarunjayanthi@gmail.com>
2021-10-23 18:19:48 -0400

    MobileFix

---
+++ Notes.js
@@ -0,0 +1,74 @@
+function playnote1(){
+	var note1 = new Audio("Notes/D2.m4a");
+	note1.play()
+}
+
+function playnote2(){
+	var note2 = new Audio("Notes/E2.m4a");
+	note2.play()
+}
+
+function playnote3(){
+	var note3 = new Audio("Notes/F2.m4a");
+	note3.play()
+}
+
+function playnote4(){
+	var note4 = new Audio("Notes/G2.m4a");
+	note4.play()
+}
+
+function playnote5(){
+	var note5 = new Audio("Notes/A2.m4a");
+	note5.play()
+}
+
+function playnote6(){
+	var note6 = new Audio("Notes/B2.m4a");
+	note6.play()
+}
+
+function playnote7(){
+	var note7 = new Audio("Notes/C3.m4a");
+	note7.play()
+}
+
+function playnote8(){
+	var note8 = new Audio("Notes/D3.m4a");
+	note8.play()
+}
+
+function playnote9(){
+	var note9 = new Audio("Notes/E3.m4a");
+	note9.play()
+}
+
+function playnote10(){
+	var note10 = new Audio("Notes/F3.m4a");
+	note10.play()
+}
+
+function playnote11(){
+	var note11 = new Audio("Notes/G3.m4a");
+	note11.play()
+}
+
+function playnote12(){
+	var note12 = new Audio("Notes/A3.m4a");
+	note12.play()
+}
+
+function playnote13(){
+	var note13 = new Audio("Notes/B3.m4a");
+	note13.play()
+}
+
+function playnote14(){
+	var note14 = new Audio("Notes/C4.m4a");
+	note14.play()
+}
+
+function playnote15(){
+	var note15 = new Audio("Notes/D4.m4a");
+	note15.play()
+}
